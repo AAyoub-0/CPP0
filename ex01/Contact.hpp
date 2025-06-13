8252e96e15e19cd5084167981d86224277694991
AAyoub <yousalama5@gmail.com>
2025-06-13 17:56:38 +0200

    feat: ex01

---
+++ ex01/Contact.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aboumall <aboumall@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/13 16:42:19 by aboumall          #+#    #+#             */
+/*   Updated: 2025/06/13 16:47:42 by aboumall         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONTACT_HPP
+# define CONTACT_HPP
+
+class Contact
+{
+	private:
+		/* data */
+	public:
+		Contact(/* args */);
+		~Contact();
+};
+
+Contact::Contact(/* args */)
+{
+}
+
+Contact::~Contact()
+{
+}
+
+
+#endif
---
+++ ex01/PhoneBook.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aboumall <aboumall@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/13 16:46:23 by aboumall          #+#    #+#             */
+/*   Updated: 2025/06/13 16:49:09 by aboumall         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONE_BOOK_HPP
+# define PHONE_BOOK_HPP
+
+class PhoneBook
+{
+	private:
+		/* data */
+	public:
+		PhoneBook();
+		~PhoneBook();
+};
+
+PhoneBook::PhoneBook(/* args */)
+{
+}
+
+PhoneBook::~PhoneBook()
+{
+}
+
+#endif
