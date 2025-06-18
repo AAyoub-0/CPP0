d13a660645cb733ad510629c1b24c3713e5c9473
AAyoub <yousalama5@gmail.com>
2025-06-18 17:48:50 +0200

    feat: ex01

--- ex01/Contact.hpp
+++ ex01/Contact.hpp
@@ -6,7 +6,7 @@
 /*   By: aboumall <aboumall@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/13 16:42:19 by aboumall          #+#    #+#             */
-/*   Updated: 2025/06/13 16:47:42 by aboumall         ###   ########.fr       */
+/*   Updated: 2025/06/18 16:47:27 by aboumall         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,9 @@
 class Contact
 {
 	private:
-		/* data */
+		char	*first_name;
+		char	*last_name;
+		char	num[15];
 	public:
 		Contact(/* args */);
 		~Contact();
--- ex01/PhoneBook.hpp
+++ ex01/PhoneBook.hpp
@@ -6,28 +6,46 @@
 /*   By: aboumall <aboumall@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/13 16:46:23 by aboumall          #+#    #+#             */
-/*   Updated: 2025/06/13 16:49:09 by aboumall         ###   ########.fr       */
+/*   Updated: 2025/06/18 16:44:17 by aboumall         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHONE_BOOK_HPP
 # define PHONE_BOOK_HPP
 
+#include "Contact.hpp"
+
 class PhoneBook
 {
 	private:
-		/* data */
+		Contact contacts[8];
+		int	index = 0;
 	public:
-		PhoneBook();
+		PhoneBook(Contact contacts[8]);
 		~PhoneBook();
+		void AddContact(Contact contact);
 };
 
-PhoneBook::PhoneBook(/* args */)
+PhoneBook::PhoneBook(Contact *contacts)
 {
+	for (int i = 0; i < 8; i++)
+	{
+		this->contacts[i] = contacts[i];
+		index++;
+	}
 }
 
-PhoneBook::~PhoneBook()
+PhoneBook::~PhoneBook() {}
+
+void PhoneBook::AddContact(Contact contact)
 {
+	if (index < 8) {
+		this->contacts[index] = contact;
+		index++;
+	} else {
+		index = 8;
+		this->contacts[index] = contact;
+	}
 }
 
 #endif
